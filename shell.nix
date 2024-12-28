7bf7cdc2d8e9e09e2b14c4a90b8d6055a3c10ecc
Rune K. Svendsen <runesvend@gmail.com>
2024-12-28 10:49:57 +0100

    CI: add more GHC versions

--- shell.nix
+++ shell.nix
@@ -7,15 +7,23 @@ let
       sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
     }) {};
 
+  pkgs1809 =
+    import (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/925ff360bc33876fdb6ff967470e34ff375ce65e.tar.gz";
+      sha256 = "1qbmp6x01ika4kdc7bhqawasnpmhyl857ldz25nmq9fsmqm1vl2s";
+    }) {};
+
   pkgs2405 =
     import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/31ac92f9628682b294026f0860e14587a09ffb4b.tar.gz";
       sha256 = "0qbyywfgjljfb4izdngxvbyvbrkilmpsmmx2a9spbwir2bcmbi14";
     }) {};
+
+  ghc = pkgs.haskell.compiler.${ghcVersion} or pkgs1809.haskell.compiler.${ghcVersion};
 in
 pkgs.mkShell {
   nativeBuildInputs = [
-    pkgs.haskell.compiler.${ghcVersion}
+    ghc
     pkgs.cabal-install
     pkgs.git
     pkgs2405.gcc9 # later versions fail with issue https://github.com/jberthold/packman/issues/17
