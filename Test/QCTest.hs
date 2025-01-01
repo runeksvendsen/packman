a42fb0e93b623c524fc1c77fe8ea40c314909717
Rune K. Svendsen <runesvend@gmail.com>
2025-01-01 10:46:34 +0100

    Tests: move away from `detailed` interface
    Use `exitcode-stdio-1.0` instead. Because these tests randomly fail for GHC 8.0.2 and 7.10.3 with this error:
    
    ```
    panic! read @TestSuiteLog ""
    CallStack (from HasCallStack):
      error, called at src/Distribution/Simple/Test/LibV09.hs:133:34 in Cabal-3.10.1.0-5sSVeMY5t4HKsrW2wqIvsr:Distribution.Simple.Test.LibV09
    ```

--- Test/QCTest.hs
+++ Test/QCTest.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE RecordWildCards, DeriveFunctor #-}
-module QCTest(tests) where
+module Main(main) where
 
 import Distribution.TestSuite
 import Test.QuickCheck
@@ -10,17 +10,39 @@ import qualified Data.Foldable as F
 import Control.Applicative
 
 import GHC.Packing
+import qualified System.Exit
+import Control.Monad (forM, unless)
 
--- use "detailed" interface: defining test instances
-tests :: IO [Test]
-tests = mapM (return . Test . uncurry (runQC 10))
+main :: IO ()
+main = do
+    putStrLn "Running all tests"
+    results <- forM mytests runTest
+    unless (and results) $ do
+        putStrLn "Some tests failed (see output above)"
+        System.Exit.exitFailure
+    where
+        runTest (name, action) = do
+            putStrLn $ "Running test '" ++ name ++ "'..."
+            b <- action
+            putStrLn $ (if b then "PASS: " else "FAIL: ") ++ name
+            return b
+
+mytests :: [(String, IO Bool)]
+mytests =
+    map
+        (fmap (runQC' 10))
         [boldTrees, foldmap square (+) 0, foldmapforce square (+) 0 ]
+    where
+        runQC' size = fmap readResult . quickCheckWithResult stdArgs{maxSize=size}
+
+        readResult Success{..} = True
+        readResult _ = False
 
 square x = x*x
 
 -- boldTrees :: (Arbitrary a, Eq a, Show a) => (String, a -> Property)
 boldTrees :: (String, RoseTree Int -> Property)
-boldTrees = ("bold trees", 
+boldTrees = ("bold trees",
              \t -> ioProperty (do t' <- duplicate t
                                   return (t == t')))
 
@@ -39,9 +61,9 @@ duplicate x = trySerialize x >>= deserialize
 
 ------------------------
 
-runQC :: (Arbitrary a, Show a, Testable p) => 
+runQC :: (Arbitrary a, Show a, Testable p) =>
          Int -> String -> (a -> p) -> TestInstance
-runQC size n prop = TestInstance 
+runQC size n prop = TestInstance
                { name = "QC test " ++ n
                , tags = [], options = []
                , setOption = \_ _ -> Right (runQC size n prop)
@@ -56,7 +78,7 @@ runQC size n prop = TestInstance
           readResult NoExpectedFailure{..} = Fail output
 
 -- our test data. Uses a number of different constructors...
-data RoseTree a 
+data RoseTree a
     = Withered a
     | Rose1 a (RoseTree a)
     | Rose2 a (RoseTree a) (RoseTree a)
@@ -70,11 +92,11 @@ data RoseTree a
 partEval :: RoseTree a -> RoseTree a
 partEval (Withered x) = x `seq` Withered x
 partEval (Rose2 x t u)  = partEval u `seq` x `seq` Rose2 x t u
-partEval (Rose4 x t u v w)  = partEval u `seq` partEval w `seq` x 
+partEval (Rose4 x t u v w)  = partEval u `seq` partEval w `seq` x
                               `seq` Rose4 x t u v w
 partEval t = t
 
-instance Foldable RoseTree 
+instance Foldable RoseTree
     where -- foldr :: (a -> b -> b) -> b -> Rosetree a -> b
           foldr f x (Withered a) = f a x
           foldr f x (Rose1 a t)  = f a (F.foldr f x t)
@@ -90,10 +112,10 @@ instance Traversable RoseTree where
     traverse f (Withered a) = Withered <$> f a
     traverse f (Rose1 a t)  = Rose1 <$> f a <*> traverse f t
     traverse f (Rose2 a t u) = Rose2 <$> f a <*> traverse f t <*> traverse f u
-    traverse f (Rose3 a t u v) 
+    traverse f (Rose3 a t u v)
         = Rose3 <$> f a <*> traverse f t <*> traverse f u <*> traverse f v
-    traverse f (Rose4 a t u v w) 
-        = Rose4 <$> f a <*> traverse f t <*> traverse f u 
+    traverse f (Rose4 a t u v w)
+        = Rose4 <$> f a <*> traverse f t <*> traverse f u
                         <*> traverse f v <*> traverse f w
     traverse f (RoseN a ts) = RoseN <$> f a <*> sequenceA (map (traverse f) ts)
 
@@ -107,17 +129,17 @@ rt n = oneof [ do t <- rt (n-1)
                   return (Rose1 x t)
              , do t <- rt (n-1); u <- rt (n-1)
                   x <- arbitrary
-                  return (Rose2 x t u) 
+                  return (Rose2 x t u)
              , do t <- rt (n-1); u <- rt (n-1); v <- rt (n-1)
                   x <- arbitrary
-                  return (Rose3 x t u v) 
+                  return (Rose3 x t u v)
              , do t <- rt (n-1); u <- rt (n-1); v <- rt (n-1); w <- rt (n-1)
                   x <- arbitrary
-                  return (Rose4 x t u v w) 
+                  return (Rose4 x t u v w)
              , do i <- choose (5,n)
                   ts <- sequence (replicate i (rt (n-1)))
                   x <- arbitrary
-                  return (RoseN x ts) 
+                  return (RoseN x ts)
              ]
 
 norm :: RoseTree a -> RoseTree a
@@ -127,4 +149,3 @@ norm (RoseN x [t,u]) = Rose2 x (norm t) (norm u)
 norm (RoseN x [t,u,v]) = Rose3 x (norm t) (norm u) (norm v)
 norm (RoseN x [t,u,v,w]) = Rose4 x (norm t) (norm u) (norm v) (norm w)
 norm t = t
-
